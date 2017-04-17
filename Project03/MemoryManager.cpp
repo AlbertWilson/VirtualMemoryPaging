bc28984d0575b5ef8249bfdd95cde93b95d575a8
Albert Wilson <Albert Wilson>
2017-04-17 19:33:53 -0400

    Updated02

--- Project03/MemoryManager.cpp
+++ Project03/MemoryManager.cpp
@@ -9,13 +9,21 @@ Project 03 Virtual Memory Paging
 #include "MemoryManager.h";
 
 
-MemoryManager::MemoryManager(ReplacementPolicy policy, unsigned int pageSize, unsigned int numFrames, unsigned int virtualAddressSpaceSize) {
+MemoryManager::MemoryManager(ReplacementPolicy policy, unsigned int pageSize, unsigned int numFrames, unsigned int virtualAddressSpaceSize)
+	: virtualMemoryManagerInterface(policy, pageSize, numFrames, virtualAddressSpaceSize)
+{
 
 	this->policy = policy;
 	this->pageSize = pageSize;
 	this->numFrames = numFrames;
 	this->virtualAddressSpaceSize = virtualAddressSpaceSize;
 	
+}
+
+unsigned long long MemoryManager::memoryAccess(unsigned long long address) {
+
+	return address;
+
 }
 
 void MemoryManager::FIFO() {
--- Project03/MemoryManager.h
+++ Project03/MemoryManager.h
@@ -8,10 +8,12 @@ Project 03 Virtual Memory Paging
 
 #include "virtualMemoryManagerInterface.hpp";
 
-class MemoryManager : virtualMemoryManagerInterface {
+class MemoryManager : virtualMemoryManagerInterface{
 
 public:
+
 	MemoryManager(ReplacementPolicy policy, unsigned int pageSize, unsigned int numFrames, unsigned int virtualAddressSpaceSize);
+	unsigned long long memoryAccess(unsigned long long address);
 	void FIFO();
 	void LRU();
 
